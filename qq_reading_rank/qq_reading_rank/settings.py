8ea8099f167aec1b7253948d8735c9aaddc38ca8
LogicPulse551 <zz318429@qq.com>
2026-05-07 22:43:03 +0800

    change road

--- qq_reading_rank/qq_reading_rank/settings.py
+++ qq_reading_rank/qq_reading_rank/settings.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[2]
-sys.path.insert(0, str(BASE_DIR.parent))
+sys.path.insert(0, str(BASE_DIR))
 
 # Scrapy settings for qq_reading_rank project
 #
